9bd285453bbcd96a5f54faeefd79ac48b5512101
Cybourgeoisie <rbenheidorn@gmail.com>
2020-06-24 16:03:51 -0400

    Removed a bunch of unneeded joins on event_transfer_owner

--- sql/schema/005_event_transfer_owner_view.sql
+++ sql/schema/005_event_transfer_owner_view.sql
@@ -1,4 +1,4 @@
-CREATE VIEW event_transfer_owner AS 
+CREATE OR REPLACE VIEW event_transfer_owner AS
 SELECT DISTINCT ON (et.contract_address, a.address, et.id)
 	et.contract_address,
 	a.address,
@@ -8,50 +8,15 @@ SELECT DISTINCT ON (et.contract_address, a.address, et.id)
 	SUM(CASE WHEN a.address = et.from THEN CASE WHEN et.value IS NOT NULL THEN et.value ELSE 1 END END) AS output
 FROM
 	event_transfer et,
-	event e,
-	log l,
-	transaction t,
-	block b,
 	(
 		SELECT "to" AS address FROM event_transfer
 		UNION
 		SELECT "from" AS address FROM event_transfer
 	) AS a
 WHERE
-	e.event_id = et.event_id AND
-	l.log_id = e.log_id AND
-	t.hash = l.transaction_hash AND
-	b.hash = t.block_hash AND
-	(et.to = a.address OR et.from = a.address)
+	et.to = a.address OR
+	et.from = a.address
 GROUP BY
 	et.contract_address,
 	a.address,
 	et.id;
-
--- NFT-only version, derived from previous blockchain middleware code
---CREATE VIEW event_transfer_owner_asset AS 
---SELECT DISTINCT ON (contract_address, id)
---	et.contract_address,
---	et.id,
---	et.to
---FROM
---	event_transfer et,
---	event e,
---	log l,
---	transaction t,
---	block b
---WHERE
---	e.event_id = et.event_id AND
---	l.log_id = e.log_id AND
---	t.hash = l.transaction_hash AND
---	b.hash = t.block_hash AND
---	et.id IS NOT NULL
---GROUP BY
---	et.contract_address,
---	et.id,
---	et.to
---ORDER BY
---	et.contract_address,
---	et.id ASC,
---	max(b.number) DESC,
---	max(l.log_index) DESC;
