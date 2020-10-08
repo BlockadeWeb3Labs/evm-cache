1c173354bba9ec87785bb0af4ba1c18a9658f59f
Cybourgeoisie <rbenheidorn@gmail.com>
2020-10-08 07:04:06 -0400

    Fixing one of my less-obvious and dumber typos.

--- sql/schema/008_asset_metadata.sql
+++ sql/schema/008_asset_metadata.sql
@@ -11,7 +11,7 @@ CREATE TABLE asset_metadata (
 CREATE INDEX asset_metadata_contract_address_idx ON "asset_metadata" ("contract_address");
 CREATE INDEX asset_metadata_contract_address_id_idx ON "asset_metadata" ("contract_address", "id");
 
---ALTER TABLE "contract_meta" ADD COLUMN "token_uri_json_inferface" JSONB;
---ALTER TABLE "contract_meta" ADD COLUMN "token_uri_json_inferface_parameters" JSONB;
+--ALTER TABLE "contract_meta" ADD COLUMN "token_uri_json_interface" JSONB;
+--ALTER TABLE "contract_meta" ADD COLUMN "token_uri_json_interface_parameters" JSONB;
 --ALTER TABLE "contract_meta" ADD COLUMN "custom_token_uri" TEXT;
 --ALTER TABLE "contract_meta" ADD COLUMN "custom_token_uri_headers" JSONB;
